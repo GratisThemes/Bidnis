da1da5a15a39eaaa5fd18c5b55281529264c2c7d
Kniffen <knifftech@gmail.com>
2021-07-07 13:23:09 +0200

    Update wp-enqueue-scripts.php

--- inc/actions/wp-enqueue-scripts.php
+++ inc/actions/wp-enqueue-scripts.php
@@ -1,37 +1,64 @@
 <?php
 /**
- * Scripts, styles and fonts
+ * The wp_enqueue_scripts hook.
  *
  * @package Bidnis
  * @since   Bidnis 2.0.0
  */
+
+/**
+ * Enqueue scripts, styles and fonts.
+ */
 function bidnis_wp_enqueue_scripts() {
-  
-  // Fonts from google
-  wp_enqueue_style( 'bidnis-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i', array(), null);
 
-  // Enqueue Font Awesome (example icon set)
-  wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/icons/fontawesome-free-5.15.3-web/css/all.min.css', array(), '5.15.3' );
+  $theme_version = wp_get_theme()->get( 'Version' );
 
-  // Theme stylesheet
-  wp_enqueue_style( 'bidnis-style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version') );
+  // Fonts from google.
+  wp_enqueue_style(
+    'bidnis-fonts',
+    'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i',
+    false,
+    $theme_version
+  );
 
-  // Theme JavaScript
-  wp_enqueue_script( 'bidnis-script', get_template_directory_uri() . '/assets/js/functions.js', false, wp_get_theme()->get('Version'), true );
+  // Enqueue Font Awesome (icon set).
+  wp_enqueue_style(
+    'font-awesome',
+    get_template_directory_uri() . '/assets/icons/fontawesome-free-5.15.3-web/css/all.min.css',
+    false,
+    '5.15.3'
+  );
 
-  // Comment-reply script
-  if ( !is_admin() && is_singular() && comments_open() && get_option('thread_comments') ) {
+  // Theme stylesheet.
+  wp_enqueue_style(
+    'bidnis-style',
+    get_stylesheet_uri(),
+    false,
+    $theme_version
+  );
+
+  // Theme JavaScript.
+  wp_enqueue_script(
+    'bidnis-script',
+    get_template_directory_uri() . '/assets/js/functions.js',
+    false,
+    wp_get_theme()->get( 'Version' ),
+    true
+  );
+
+  // Comment-reply script.
+  if ( ! is_admin() && is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
     wp_enqueue_script( 'comment-reply' );
   }
 
-  // Custom styles
-  $options = [
+  // Custom styles.
+  $options = array(
     '#' . get_background_color(),
-    get_theme_mod( 'color_scheme',      '#FFD200' ),
-    get_theme_mod( 'text_color',        '#191919' ),
+    get_theme_mod( 'color_scheme', '#FFD200' ),
+    get_theme_mod( 'text_color', '#191919' ),
     get_theme_mod( 'header-text-color', '#191919' ),
-    get_theme_mod( 'link_color',        '#191919' ),
-  ];
+    get_theme_mod( 'link_color', '#191919' ),
+  );
 
   $css = '
     :root {
@@ -44,7 +71,6 @@ function bidnis_wp_enqueue_scripts() {
     }
   ';
 
-  
   wp_add_inline_style( 'bidnis-style', vsprintf( $css, $options ) );
 
 }
