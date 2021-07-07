e571c983d30e0aec16313034e3bc53290e817c07
Kniffen <knifftech@gmail.com>
2021-07-07 14:02:55 +0200

    Fixed phone and email icons

--- inc/theme-functions/phone-email.php
+++ inc/theme-functions/phone-email.php
@@ -1,17 +1,28 @@
 <?php
 /**
-  * Phone and E-mail
-  *
-  * @since Bidnis 1.0.0
-  */
+ * Phone and E-mail
+ *
+ * @package Bidnis
+ * @since   Bidnis 2.0.0
+ */
+
+/**
+ * Phone and E-mail
+ */
 function bidnis_phone_email() { ?>
   <div id="bidnis-phone-email">
-    <?php if( get_theme_mod('phone') ): ?>
-      <a id="bidnis-phone" href="tel:<?php echo esc_url( get_theme_mod('phone') ); ?>"><?php echo esc_html( get_theme_mod('phone') ); ?></a>
+    <?php if ( get_theme_mod( 'phone' ) ) : ?>
+      <div id="bidnis-phone">
+        <i class="fa fa-phone"></i>
+        <a href="tel:<?php echo esc_url( get_theme_mod( 'phone' ) ); ?>"><?php echo esc_html( get_theme_mod( 'phone' ) ); ?></a>
+    </div>
     <?php endif; ?>
 
-    <?php if( get_theme_mod('email') ): ?>
-      <a id="bidnis-email" href="mailto:<?php echo esc_attr( get_theme_mod('email') ); ?>" target="_top"><?php echo esc_html( get_theme_mod('email') ); ?></a>
+    <?php if ( get_theme_mod( 'email' ) ) : ?>
+      <div id="bidnis-email">
+        <i class="fa fa-envelope"></i>
+        <a href="mailto:<?php echo esc_attr( get_theme_mod( 'email' ) ); ?>" target="_top"><?php echo esc_html( get_theme_mod( 'email' ) ); ?></a>
+    </div>
     <?php endif; ?>
   </div>
 <?php } ?>
