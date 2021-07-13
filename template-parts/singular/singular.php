e2296b735a413deef97ab61f7c43ae65676d9cad
Kniffen <knifftech@gmail.com>
2021-07-13 09:16:51 +0200

    Update singular.php

--- template-parts/singular/singular.php
+++ template-parts/singular/singular.php
@@ -41,8 +41,8 @@
   <?php
   the_post_navigation(
     array(
-      'prev_text' => '<span class="screen-reader-text">' . __( 'Previous post', 'bidnis' ) . '</span><span>' . __( 'Previous', 'bidnis' ) . '</span><span>%title</span>',
-      'next_text' => '<span class="screen-reader-text">' . __( 'Next post', 'bidnis' ) . '</span><span>' . __( 'Next', 'bidnis' ) . '</span><span>%title</span>',
+      'prev_text' => '<span class="screen-reader-text">' . __( 'Previous post', 'bidnis' ) . '</span><span>%title</span>',
+      'next_text' => '<span class="screen-reader-text">' . __( 'Next post', 'bidnis' ) . '</span><span>%title</span>',
     )
   );
   ?>
